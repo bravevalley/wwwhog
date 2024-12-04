3ce5fb60f0bb829fd2fe81b03f0557d5889d7b49
bravevalley <dassyareg@gmail.com>
2024-12-04 09:29:49 +0100

    updated cmd for username get

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,17 +5,14 @@ import (
 	"os/exec"
 )
 
+func Getlisting(username, server_id string) (string, error) {
 
-func Getlisting(username string) (string, error) {
-	
-	cmd, err := exec.Command("/usr/local/bin/fetch_backup.sh").Output()
+	cmd, err := exec.Command("/usr/local/bin/fetch_backup.sh", "username", "server_id").Output()
 
 	if err != nil {
-		return "", fmt.Errorf("%s error: %v\n", "/usr/local/bin/fetch_backup.sh", err)
+		return "", fmt.Errorf("%s error: %v", "/usr/local/bin/fetch_backup.sh", err)
 	}
-	
+
 	return string(cmd), nil
 
-	
 }
-
--- main.go
+++ main.go
@@ -3,17 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"com.go54/rbso/cmd"
 )
 
 
+var (
+	Get_req = "GET"
+ 	Keep_req = "KEEP"
+ 	hstnme string
+ 	out string
+ 	err error
+)
+
 func main() {
-	
-	out, err := cmd.Getlisting("dovemini")
-	if err != nil {
-		log.Fatal(err)
+
+	if len(os.Args) > 4 {
+		log.Fatalf("Wrong!") // TODO: Change later
 	}
+
+
+
+
+	switch request := strings.ToUpper(os.Args[1]); request {
+	case Get_req:
+		var un = strings.ToLower(os.Args[2])
+		hstnme = strings.ToLower(os.Args[3])
+
+		out, err = cmd.Getlisting(un, hstnme)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case Keep_req:
+		var metadata = strings.ToLower(os.Args[3])
+		hstnme = strings.ToLower(os.Args[2])
+
+		out, err = cmd.Keepdata(hstnme, metadata)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatalf("Action \"%v\" unknown; kindly use GET or KEEP", request)
+
+	}
+
 
 	fmt.Println(out)
 }
